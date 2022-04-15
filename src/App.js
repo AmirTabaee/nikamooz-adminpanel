0e454b201ec20d2828ffbf237365b03d9be96607
AmirTabaee <sir.amir.71@gmail.com>
2022-04-15 14:54:12 +0430

    project going to be initialized again

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import "./App.css";
-import { Switch } from "react-router-dom";
 import { AdminLayout } from "./layouts/admin-layout/AdminLayout";
-import { Route } from "react-router-dom";
-import PrivateRoute from "./utils/PrivateRoute";
-import { Login } from "./components/auth/Login";
+
 //alias
 
 function App() {
-   return (
-      <Switch>
-         <Route path="/login" component={Login} />
-         <PrivateRoute path="/" component={AdminLayout} />
-      </Switch>
-   );
+   return <AdminLayout />;
 }
 
 export default App;
--- src/layouts/admin-layout/Sidebar.js
+++ src/layouts/admin-layout/Sidebar.js
@@ -1,49 +1,55 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import logo from '../../assets/images/logo-nikamooz.png'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/images/logo-nikamooz.png";
 
 export const Sidebar = () => {
-    return (
-        <div className="sidebar" data-color="white" data-active-color="danger">
-            <div className="logo">
-                <NavLink className="simple-text logo-normal" to="/">
-                    <img src={logo} width="64%" />
-                </NavLink>
-            </div>
-            <div className="sidebar-wrapper">
-                <ul className="nav">
-                    <li>
-                        <NavLink exact activeClassName="active" className="simple-text logo-normal" to="/">
-                            <i className="nc-icon nc-bank"></i>
-                        داشبورد
-                    </NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName="active" className="simple-text logo-normal" to="/products">
-                            <i className="nc-icon nc-tile-56"></i>
-                        محصولات
-                    </NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName="active" className="simple-text logo-normal" to="/formdata">
-                            <i className="nc-icon nc-diamond"></i>
-                        نمونه فرم
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName="active" className="simple-text logo-normal" to="/react-hook-form">
-                            <i className="nc-icon nc-diamond"></i>
-                        نمونه فرم با React Hook
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink activeClassName="active" className="simple-text logo-normal" to="/formik-sample">
-                            <i className="nc-icon nc-diamond"></i>
-                       Formik
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+   return (
+      <div className="sidebar" data-color="white" data-active-color="danger">
+         <div className="logo">
+            <NavLink className="simple-text logo-normal" to="/">
+               <img src={logo} width="64%" />
+            </NavLink>
+         </div>
+         <div className="sidebar-wrapper">
+            <ul className="nav">
+               <li>
+                  <NavLink exact activeClassName="active" className="simple-text logo-normal" to="/">
+                     <i className="nc-icon nc-bank"></i>
+                     داشبورد
+                  </NavLink>
+               </li>
+               <li>
+                  <NavLink activeClassName="active" className="simple-text logo-normal" to="/products">
+                     <i className="nc-icon nc-tile-56"></i>
+                     محصولات
+                  </NavLink>
+               </li>
+               <li>
+                  <NavLink activeClassName="active" className="simple-text logo-normal" to="/post">
+                     <i className="nc-icon nc-tile-56"></i>
+                     پست ها
+                  </NavLink>
+               </li>
+               <li>
+                  <NavLink activeClassName="active" className="simple-text logo-normal" to="/formdata">
+                     <i className="nc-icon nc-diamond"></i>
+                     نمونه فرم
+                  </NavLink>
+               </li>
+               <li>
+                  <NavLink activeClassName="active" className="simple-text logo-normal" to="/react-hook-form">
+                     <i className="nc-icon nc-diamond"></i>
+                     نمونه فرم با React Hook
+                  </NavLink>
+               </li>
+               <li>
+                  <NavLink activeClassName="active" className="simple-text logo-normal" to="/formik-sample">
+                     <i className="nc-icon nc-diamond"></i>
+                     Formik
+                  </NavLink>
+               </li>
+            </ul>
+         </div>
+      </div>
+   );
+};
